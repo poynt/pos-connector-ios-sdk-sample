b5e38439abc38b79931bc05eca4da436d8df4bfc
Eric McConkie <eric@ericmcconkie.com>
2016-08-01 23:08:02 -0700

    update lib remove pointer to crypto

--- Pods/Target Support Files/Pods-iOStoPOSExample/Pods-iOStoPOSExample-frameworks.sh
+++ Pods/Target Support Files/Pods-iOStoPOSExample/Pods-iOStoPOSExample-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-iOStoPOSExample/Amplitude_iOS.framework"
   install_framework "Pods-iOStoPOSExample/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-iOStoPOSExample/Amplitude_iOS.framework"
   install_framework "Pods-iOStoPOSExample/MBProgressHUD.framework"
 fi
